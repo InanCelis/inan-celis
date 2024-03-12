24edec283f68e3ef7e26ed7d48a5c266938cac4b
InanCelis <celisinan@gmail.com>
2024-03-12 20:58:52 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Inan Celis</h1>
+        <a
+          className="App-link"
+          href="https://inan-celis.surge.sh/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Know more about Inan
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
