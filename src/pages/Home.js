ae979f62495a2f0bc723d40920b78523884f31c4
InanCelis <celisinan@gmail.com>
2024-03-14 04:24:18 +0800

    Added changed image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,30 +2,11 @@ import React from 'react';
 import Layout from '../Components/Layout/Layout';
 import { useHelperFunction } from '../utils/helperFuntion';
 import '../assets/styles/home.scss';
-import Me from '../assets/images/me.png';
+import Me from '../assets/images/meee.png';
 function Home() {
   useHelperFunction('');
   return (
     <Layout>
-      {/* <div className='homePage'>
-        <div className="text-container">
-          <h1>INAN CELIS</h1>
-        </div>
-      </div> */}
-
-      {/* <div class="row container">
-        <div class="col-lg-6 offset-lg-1">
-          <img src={Me} class="my-image"/>
-        </div>
-        <div class="col-lg-5 banner-text">
-          <h1>Website</h1>
-          <h2>Developer</h2>
-          <p class="text-muted">
-            A WEBSITE AND FRONT-END DEVELOPER WHO LOVES DESIGNING WEBSITES AND MAKING THE COMPLEX PROBLEM INTO SIMPLE.
-          </p>
-        </div>
-      </div> */}
-
       <div className='homepage container'>
         <div className='content'>
           <div className='image'>
