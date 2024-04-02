5769a84e3bcd164cca0bad77dab88a382dfe841d
InanCelis <celisinan@gmail.com>
2024-04-02 18:20:29 +0800

    Added popup

---
+++ src/Components/Slice/AlertPopup.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../../assets/styles/AlertPopup.scss'; // Make sure to create this CSS file for styling
+
+const AlertPopup = ({ alertInfo, onClose }) => {
+  if (!alertInfo.isVisible) return null;
+
+  return (
+    <div className="alert-backdrop">
+      <div className="alert-box">
+        <button onClick={onClose} className='close'><i class="fa-solid fa-xmark"></i></button>
+        <div className='type'>
+          { alertInfo.type === 'success'? (
+            <>
+              <i class="fa-regular fa-circle-check text-success"></i>
+              <h3 className='pt-2'>Success!</h3>
+            </>
+          ): 
+          (
+            <>
+              <i class="fa-solid fa-exclamation error"></i>
+              <h3 className='pt-2'>Opps, error!</h3>
+            </>
+          )}
+        </div>
+        <p>{alertInfo.message}</p>
+        <button onClick={onClose} className='link link-full'>Okay</button>
+      </div>
+    </div>
+  );
+};
+
+export default AlertPopup;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,57 +1,88 @@
 
 
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from 'emailjs-com';
 
 import Layout from '../Components/Layout/Layout';
 import { useHelperFunction } from '../utils/helperFuntion';
 import LineClipPath from '../Components/Slice/LineClipPath';
 import '../assets/styles/contact.scss';
-function Contact() {
 
-    const [fieldValue, setFieldValue] = React.useState('');
-    const handleBlur = (e) => setFieldValue(e.target.value);
+import AlertPopup from '../Components/Slice/AlertPopup';
+function Contact() {
+    const [alert, setAlert] = useState({message: '', type: '', isVisible: false});
+    //form fields   
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
     
     useHelperFunction('Contact |');
 
     const form = useRef();
-
     const sendEmail = (e) => {
         e.preventDefault();
-
+        
         emailjs.sendForm('service_ptae8fe', 'template_gq5izz6', form.current, 'w-rjpWx0cbWeWkXV6')
         .then((result) => {
-            console.log(result.text);
-            alert("Email sent successfully!");
+            resetForm();
+            setAlert({message: 'Email Sent Successfully', type: 'success', isVisible: true});
         }, (error) => {
-            console.log(error.text);
-            alert("Failed to send the email.");
+            setAlert({message: 'Failed to send the message.', type: 'error', isVisible: true});
         });
     };
+
+    const resetForm = () => {
+        setName('');
+        setEmail('');
+        setMessage('');
+    }
+
     return (
         <Layout>
-        <div className=' '>
+            <AlertPopup
+                alertInfo={alert}
+                onClose={() => setAlert(alert.isVisible = false)}
+            />
             <div className=' container contact'>
                 <div className="form-container">
                     <h3 className='font-weight-bold pb-3'>Say hello!</h3>
                     <LineClipPath xtraClass={'grid-100 pb-4'}/>
                     <form ref={form} onSubmit={sendEmail}>
                         <div className="group">
-                            <input type="text" required className='form-input' name='name'/>
-                            <span className="highlight"></span>
+                            <input type="text" 
+                                required 
+                                className='form-input' 
+                                name='name' 
+                                onChange={(e) => setName(e.target.value)} 
+                                value={name}/>
+
                             <span className="bar"></span>
                             <label>Name</label>
                         </div>
                         <div class="group">
-                            <input type="email" required  className='form-input' placeholder='' onChange={handleBlur} value={fieldValue}/>
-                            <span className="highlight"></span>
+                            <input type="email" 
+                                required 
+                                name='email' 
+                                className='form-input' 
+                                placeholder='' 
+                                onChange={(e) => setEmail(e.target.value)} 
+                                value={email}/>
+                                
                             <span className="bar"></span>
                             <label>Your Email</label>
-                            <span className="validation-text">Please enter a valid email address.</span>
+                            <span className="validation-text">
+                                <i class="fa-solid fa-triangle-exclamation"></i> Please enter a valid email address.
+                            </span>
                         </div>
                         <div className="group">
-                            <textarea  id="message" name="message" rows="0" className='form-input' placeholder=''></textarea>
-                            <span className="highlight"></span>
+                            <textarea required 
+                                id="message" 
+                                name="message" 
+                                rows="0" 
+                                className='form-input'  
+                                onChange={(e) => setMessage(e.target.value)} 
+                                placeholder='' 
+                                value={message}></textarea>
                             <span className="bar"></span>
                             <label>Message</label>
                         </div>
@@ -60,7 +91,6 @@ function Contact() {
                     </form>
                 </div>
             </div>
-        </div>
         </Layout>
     );
 }
