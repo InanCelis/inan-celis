cbaca93be8abcae0f136027106dd27045b1615a4
inancelis <celisinan@gmail.com>
2025-01-04 21:22:10 +0800

    Updated the resume link

--- src/Components/Slice/ResumeButton.js
+++ src/Components/Slice/ResumeButton.js
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 function ResumeButton() {
     return (
-          <NavLink target={"_blank"} to={`https://drive.google.com/file/d/1PJiZ4KcnU-VrPsbb0Odh0o08HFbMTXbT/view?usp=sharing`} className="button-50 button" >
+          <NavLink target={"_blank"} to={`https://drive.google.com/file/d/1k7tuhuZ5qBjYdXkp_vtbkKB9jPulz6J4/view?usp=sharing`} className="button-50 button" >
               View My Resume &nbsp;<i className="fa-solid fa-arrow-right text-center"></i>
           </NavLink>
     )
